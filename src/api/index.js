5ad3e1e54a14af8a3e07642c2de9204947db650a
David Szabo <david.szabo97@gmail.com>
2020-03-15 22:59:47 +0100

    Fix environment variables

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.API_BASE_URL || 'http://localhost:3001'
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3001'
 
 export const fetchData = async () => {
   const response = await window.fetch(`${BASE_URL}/covid-19-data`)
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -98,7 +98,7 @@ function App() {
         <Map
           zoom={zoom}
           places={places}
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAP_API_KEY}`}
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAP_API_KEY}`}
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `100vh` }} />}
           mapElement={<div style={{ height: `100%` }} />}
